82bb29d52f4a5479fc046b8bd622b63cb3ab0ec3
Iwo Czartowski <iwo@czart.net>
2025-01-06 15:26:55 +0100

    xd

--- client-app/src/app/specie/service/specie-service.service.ts
+++ client-app/src/app/specie/service/specie-service.service.ts
@@ -9,7 +9,7 @@ import { map }  from 'rxjs/operators';
 })
 
 export class SpecieService {
-  private apiUrl = 'http://localhost:8080/api/species';
+  private apiUrl = '/api/species';
   
   constructor(private http: HttpClient) { }
 
